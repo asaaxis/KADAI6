0322c2f8711adc4ded8873eb7f88b7b10e2d784b
akiasa oshima <asaolr4@gmail.com>
2022-09-30 18:25:36 +0900

    フィードバックメッセージの機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to posts_path
+      redirect_to posts_path, notice: "つぶやきを作成しました！"
     else
       render :new
     end
